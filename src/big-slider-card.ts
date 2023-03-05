af96c5b26f4f3dea83bfe4f8975d1b2aa860f3bc
Nicolae Farmache <gitlab@nicu.ro>
2023-03-05 19:12:24 +0100

    Fixed slider visible outside container

--- src/big-slider-card.ts
+++ src/big-slider-card.ts
@@ -502,6 +502,7 @@ export class BigSliderCard extends GestureEventListeners(LitElement) {
         position: relative;
         overflow: hidden;
         background: var(--card-background-color);
+        z-index: 1; //fix safari bug with filter transition https://stackoverflow.com/a/27935035
       }
 
       .hide {
