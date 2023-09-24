f59c51e0c4f45f11e64b3db6f446f472bd9bb6a4
Nicolae Farmache <gitlab@nicu.ro>
2023-09-25 01:29:44 +0200

    bump version

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = '1.1.2';
+export const CARD_VERSION = '1.1.3';
 export const DEFAULT_ATTRIBUTE = 'brightness';
 export const SETTLE_TIME = 3000;
 export const HOLD_TIME = 600;
