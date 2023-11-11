b317484d80e92a6bd4b06f988eeeb0ee224029ea
Nicolae Farmache <gitlab@nicu.ro>
2023-11-11 17:50:12 +0100

    get version from package.json

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,5 @@
 import { BigSliderCardConfig } from "./types";
 
-export const CARD_VERSION = '1.2.0';
 export const DEFAULT_ATTRIBUTE = 'brightness';
 export const SETTLE_TIME = 3000;
 export const HOLD_TIME = 600;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { BigSliderCard } from './big-slider-card';
-import { CARD_VERSION } from './const';
+import { version } from '../package.json';
 import { localize } from './localize/localize';
 
 /* eslint no-console: 0 */
 console.info(
-  `%c  BIG-SLIDER-CARD \n%c  ${localize('common.version')} ${CARD_VERSION}    `,
+  `%c  BIG-SLIDER-CARD \n%c  ${localize('common.version')} ${version}    `,
   'color: orange; font-weight: bold; background: black',
   'color: white; font-weight: bold; background: dimgray',
 );
